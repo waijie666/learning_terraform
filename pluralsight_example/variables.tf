f3d8f9d7d5656c1bed975fb2f2e4d9b0edb4b3f8
waijie666 <suicune5050@hotmail.com>
2023-02-13 22:42:26 +0800

    Added pluralsight terraform practice

---
+++ pluralsight_example/variables.tf
@@ -0,0 +1,21 @@
+variable "aws_region" {
+  type = string
+  description = "AWS Region to use for resources"
+  default = "ap-southeast-1"
+}
+
+variable "company" {
+  type        = string
+  description = "Company name for resource tagging"
+  default     = "Globomantics"
+}
+
+variable "project" {
+  type        = string
+  description = "Project name for resource tagging"
+}
+
+variable "billing_code" {
+  type        = string
+  description = "Billing code for resource tagging"
+}
